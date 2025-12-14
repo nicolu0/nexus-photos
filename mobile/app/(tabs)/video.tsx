57fc7dfe785f921fc2a29d7aa257256452e3f55c
nicolu0 <nicoluo@gmail.com>
2025-12-14 01:19:52 -0800

    feat: basic camera

--- mobile/app/(tabs)/video.tsx
+++ mobile/app/(tabs)/video.tsx
@@ -1,15 +1,36 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, Image, Alert } from 'react-native';
-import { Camera, useCameraDevices, useCameraPermission } from 'react-native-vision-camera';
+import { View, Text, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission, useCameraFormat } from 'react-native-vision-camera';
 
 export default function VideoScreen() {
-	const device = useCameraDevice('back')
-	const { hasPermission } = useCameraPermission()
+	const device = useCameraDevice('back');
+	const { hasPermission } = useCameraPermission();
+	const format = useCameraFormat(device, [{ fps: 240 }]);
+
+	if (!hasPermission) {
+		return (
+			<View>
+				<Text>Requesting camera permission...</Text>
+			</View>
+		);
+	}
+
+	if (!device) {
+		return (
+			<View>
+				<Text>No camera device found.</Text>
+			</View>
+		);
+	}
 
     return (
 		<Camera
-			{...props}
+			style={StyleSheet.absoluteFill}
+			device={device}
+			isActive={true}
 			video={true}
+			format={format}
+			fps={60}
 		/>
     );
 }
